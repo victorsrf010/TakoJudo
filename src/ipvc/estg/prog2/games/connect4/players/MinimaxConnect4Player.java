75f25cc0764496c7602b958760135e9d80f35eac
Luís Teófilo <luisfgteofilo@gmail.com>
2021-03-29 11:41:01 +0100

    Improve heuristic. Add alpha beta prunning

--- src/ipvc/estg/prog2/games/connect4/players/MinimaxConnect4Player.java
+++ src/ipvc/estg/prog2/games/connect4/players/MinimaxConnect4Player.java
@@ -65,10 +65,61 @@ public class MinimaxConnect4Player extends Connect4Player {
             }
         }
 
+        //check upward diagonal
+        for(int row = 3; row < state.getnRows(); row++){
+            for(int col = 0; col < state.getnCols() - 3; col++){
+
+                int seq1 = (grid[row][col] == this.getCurrentPos()?1:0) +
+                        (grid[row-1][col+1] == this.getCurrentPos()?1:0) +
+                        (grid[row-2][col+2] == this.getCurrentPos()?1:0);
+
+                int seq2 = (grid[row-1][col+1] == this.getCurrentPos()?1:0) +
+                        (grid[row-2][col+2] == this.getCurrentPos()?1:0) +
+                        (grid[row-3][col+3] == this.getCurrentPos()?1:0);
+
+                if(seq1 > longest) {
+                    longest = seq1;
+                }
+
+                if(seq2 > longest) {
+                    longest = seq2;
+                }
+            }
+        }
+        //check downward diagonal
+        for(int row = 0; row < state.getnRows() - 3; row++){
+            for(int col = 0; col < state.getnCols() - 3; col++){
+                int seq1 = (grid[row][col] == this.getCurrentPos()?1:0)  +
+                        (grid[row+1][col+1] == this.getCurrentPos()?1:0) +
+                        (grid[row+2][col+2] == this.getCurrentPos()?1:0);
+
+                int seq2 = (grid[row+1][col+1] == this.getCurrentPos() ? 1:0) +
+                        (grid[row+2][col+2] == this.getCurrentPos() ? 1:0) +
+                        (grid[row+3][col+3] == this.getCurrentPos() ? 1:0);
+
+                if(seq1 > longest) {
+                    longest = seq1;
+                }
+
+                if(seq2 > longest) {
+                    longest = seq2;
+                }
+            }
+        }
+
         return longest;
     }
 
-    private int minimax(Connect4State state, int depth, boolean initial) {
+    /**
+     * Efetua uma pesquisa minimax (recursivamente)
+     * @param state estado atual sobre o qual a pesquisa deve ser feita
+     * @param depth profundidade máxima de pesquisa
+     * @param alpha parametro alpha para pruning
+     * @param beta parametro beta para pruning
+     * @param isInitialNode se verdadeiro, a função retorna a posição com maior valor esperado, caso contrário retorna o valor esperado da melhor ação
+     * @return
+     */
+    private int minimax(Connect4State state, int depth, int alpha, int beta, boolean isInitialNode) {
         // verificar se o jogo terminou (empate, vitória ou derrota)
         if(state.isFinished()) {
             Connect4Result result = state.getResult(this.getCurrentPos());
@@ -89,45 +140,55 @@ public class MinimaxConnect4Player extends Connect4Player {
 
         // se for o minimax a jogar
         if(this.getCurrentPos() == state.getActingPlayer()) {
-            int a = Integer.MIN_VALUE;
+            int value = Integer.MIN_VALUE;
             int selectedPos = -1;
 
             for(int pos = 0; pos < state.getnCols(); ++pos) {
                 Connect4Action action = new Connect4Action(pos);
                 if(state.validateAction(action)) {
-                    int previousA = a;
+                    int previousA = value;
                     Connect4State nextState = state.getClone();
                     nextState.play(action);
-                    a = Math.max(a, minimax(nextState, depth - 1, false));
-                    if(a >= previousA) {
+                    value = Math.max(value, minimax(nextState, depth - 1, alpha, beta,false));
+                    alpha = Math.max(alpha, value);
+
+                    if(value >= previousA) {
                         selectedPos = pos;
+                    }
+
+                    if(alpha >= beta) {
+                        break;
                     }
                 }
             }
 
-            if(initial) {
+            if(isInitialNode) {
                 return selectedPos;
             }
 
-            return a;
+            return value;
         } else { // adversário a jogar
-            int a = Integer.MAX_VALUE;
+            int value = Integer.MAX_VALUE;
             for(int pos = 0; pos < state.getnCols(); ++pos) {
                 Connect4Action action = new Connect4Action(pos);
                 if(state.validateAction(action)) {
                     Connect4State nextState = state.getClone();
                     nextState.play(action);
-                    a = Math.min(a, minimax(nextState, depth - 1, false));
+                    value = Math.min(value, minimax(nextState, depth - 1, alpha, beta, false));
+                    beta = Math.min(beta, value);
+                    if(beta <= alpha) {
+                        break;
+                    }
                 }
             }
 
-            return a;
+            return value;
         }
     }
 
     @Override
     public Connect4Action getAction(Connect4State state) {
-        return new Connect4Action(minimax(state, 5, true));
+        return new Connect4Action(minimax(state, 5, Integer.MIN_VALUE, Integer.MAX_VALUE, true));
     }
 
     @Override
