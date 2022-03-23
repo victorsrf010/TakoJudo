f063a2a289b0e354c1c71c884ca4aa812c030d36
Luís Teófilo <luisfgteofilo@gmail.com>
2022-03-23 19:24:59 +0000

    Add initial python version

---
+++ src-python/main.py
@@ -0,0 +1,26 @@
+# to be swapped by the real class
+from games.game_simulator import GameSimulator
+from games.player import Player
+
+
+def run_simulation(desc: str, simulator: GameSimulator, iterations: int):
+    print("todo")
+    # ProgressBar pb = new ProgressBar(desc, iterations);
+    # for (long i = 0; i < iterations; ++i, pb.step()) {
+    # simulator.changePlayerPositions();
+    # simulator.runSimulation();
+    # }
+    # pb.stepTo(iterations);
+    # pb.close();
+
+    # System.out.println("Results for the game:");
+    # simulator.printStats();
+
+
+def main():
+    print("ESTG IA Games Simulator")
+
+
+
+if __name__ == "__main__":
+    main()
